21778233e039beaa9881e60ac045cdc70aaee361
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-16 11:43:57 +0200

    Added splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
@@ -10,8 +11,6 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: SplashView(),
-    );
+    return GetMaterialApp(home: SplashView());
   }
 }
