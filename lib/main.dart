51ad1584943ac0d8de3d679f323ee15da8876dc7
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-16 11:36:36 +0200

    Added get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -9,6 +10,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
