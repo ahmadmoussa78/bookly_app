c2c63eb173e81b0d877b6c2dc43abf3c1b9eefae
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-20 08:57:03 +0200

    Added featured list view to home body

---
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeautredListViewItem extends StatelessWidget {
+  const FeautredListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.red,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
+          ),
+        ),
+      ),
+    );
+  }
+}
