e5bb2dca2e4e67fe1ace918a1f8b84aa24b3bf3a
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-20 09:16:49 +0200

    Created Styles file and added title to home view

--- lib/features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -9,9 +9,10 @@ class FeautredListViewItem extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 2.7 / 4,
       child: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
           color: Colors.red,
-          image: DecorationImage(
+          image: const DecorationImage(
             fit: BoxFit.fill,
             image: AssetImage(AssetsData.testImage),
           ),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,18 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(children: [CustomAppBar(), FeaturedBooksListView()]),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomAppBar(),
+            FeaturedBooksListView(),
+            SizedBox(height: 50),
+            Text("Best Seller", style: Styles.titleMeduim),
+          ],
+        ),
+      ),
     );
   }
 }
