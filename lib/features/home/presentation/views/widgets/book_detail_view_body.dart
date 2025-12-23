28470d46f48b19e393ba1840abf5104ab7fe72c5
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-23 07:34:39 +0200

    Added book item to bokk details view

--- lib/features/home/presentation/views/widgets/book_detail_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detail_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailViewBody extends StatelessWidget {
@@ -6,6 +7,15 @@ class BookDetailViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomBookDetailsAppBAr()]);
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        const CustomBookDetailsAppBAr(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .17),
+          child: const CustomBookImage(),
+        ),
+      ],
+    );
   }
 }
