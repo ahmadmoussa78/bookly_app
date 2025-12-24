a419f992c20c569202434870144dbdd248ec50bd
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-24 08:12:33 +0200

    Created Search text field for search view

---
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search",
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: const Opacity(
+            opacity: .6,
+            child: Icon(FontAwesomeIcons.magnifyingGlass, size: 28),
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
