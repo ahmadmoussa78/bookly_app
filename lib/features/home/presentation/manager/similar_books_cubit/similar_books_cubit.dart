789f84e5cc7625ae0db6ec655e3a8f221637b3ae
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-01-02 09:04:48 +0200

    Created Similar Books Cubit

---
+++ lib/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) {
+        emit(SimilarBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(SimilarBooksSuccess(books));
+      },
+    );
+  }
+}
